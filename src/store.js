b8e99b818cbc569617867193e76205819b55b941
Boa <boa.matule@gmail.com>
2020-08-04 23:21:28 +0200

    Installs  redux-thunk redux-devtools-extension @babel/runtime  @babel/plugin-transform-runtime and updates the babelrc folder.

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { todos } from './todos/reducers';
 
 const reducers = {
