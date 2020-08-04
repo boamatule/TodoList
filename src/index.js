d73fba83a6f7a1fd7795c6c2fd5996198aa1d353
Boa <boa.matule@gmail.com>
2020-08-04 02:22:04 +0200

    Configures the webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './App.js':
+import App from './App.js';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+  <App />
+, document.getElementById('root'));
