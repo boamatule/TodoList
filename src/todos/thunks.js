33198cde90da6d70e21c3772e885ae64d370cfba
Boa <boa.matule@gmail.com>
2020-08-08 23:23:00 +0200

    Creates a catch block for the errors from the server while fetching our tods and modify the message to be display...

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,13 +1,18 @@
 import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from './actions';
 
 export const loadTodos = () => async (dispatch, getState) => {
-  dispatch{loadTodosInProgress()};
-  const response = await fetch('http://localhost:8080/todos');
-  const todos = await response.json();
-
-  dispatch{loadTodosSuccess(todos)};
+  try {
+    dispatch(loadTodosInProgress());
+    const response = await fetch('http://localhost:8080/todos');
+    const todos = await response.json();
+  
+    dispatch(loadTodosSuccess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailure());
+    dispatch(displayAlert(e));
+  }
 }
 
 export const displayAlert = text => {
-  alert
-}
+  alert(text);
+};
