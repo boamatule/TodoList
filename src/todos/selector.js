a2fc53710115eb200c3b438c34d6f5c88c9a5177
Boa <boa.matule@gmail.com>
2020-08-13 00:04:36 +0200

    Installs reselect and creates  getIncompleteTodos. WIP

--- src/todos/selector.js
+++ src/todos/selector.js
@@ -1,2 +1,10 @@
+import { createSelector } from 'reselect';
+import { createStore } from 'redux';
+
 export const getTodos = state => state.todos;
-export consy getTodosLoading = state => state.isLoading;
+export const getTodosLoading = state => state.isLoading;
+
+export const getIncompleteTodos = createSelector(
+  getTodos,
+  (todos) => todos.filter(todo => !todo.isCompleted),
+);
